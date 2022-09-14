96f331d754fb9b59fbb1057987ea3b0969b91039
Yuuuk94 <yuuuk94@gmail.com>
2022-09-15 04:27:30 +0900

    totalAmount 변수 제거하기

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const play = require("./plays.json");
 const invoices = require("./invoices.json");
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역(고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -10,13 +9,20 @@ function statement(invoice, plays) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
   function totalVolumeCredits() {
     let volumeCredits = 0;
