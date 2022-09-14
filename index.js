06e93d525fc9d3af3260bd5c88dc4dfde3c99774
Yuuuk94 <yuuuk94@gmail.com>
2022-09-15 05:30:03 +0900

    중간 데이터 생성 함수 createStatementData.js

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 const play = require("./plays.json");
 const invoices = require("./invoices.json");
+const createStatementData = require("./createStatementData.js");
 
 function statement(invoice, plays) {
-  let result = `청구 내역(고객명: ${invoice.customer})\n`;
+  return renderPlainText(createStatementData(invoice, plays));
+}
 
-  for (let perf of invoice.performances) {
+function renderPlainText(data) {
+  let result = `청구 내역(고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += `${perf.play.name}: ${usd(perf.amount)} (${
       perf.audience
     }석)\n`;
   }
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
 
   return result;
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-    return volumeCredits;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
@@ -38,43 +27,6 @@ function statement(invoice, plays) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    // 포인트를 적립한다.
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-      case "tragedy": //비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy": //희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-    }
-    return result;
   }
 }
 
